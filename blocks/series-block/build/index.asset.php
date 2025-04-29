23a84facb35f4f0b52d65133b7754c5e781dddc8
Chris Garrett <chris@chrisg.com>
2025-04-29 09:10:22 +0100

    Fix block editor script loading by updating block.json and building assets

--- blocks/series-block/build/index.asset.php
+++ blocks/series-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b9aac7670de47389d5db');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a7109b22a19ec22b1d2e');
